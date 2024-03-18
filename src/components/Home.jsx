09548aecab644358405b844b06d1053fe4d490b0
kps21 <121485662+kps21@users.noreply.github.com>
2024-03-18 10:21:02 +0530

    Update Home.jsx
    removed unnecessary codes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import WeatherData from "./WeatherData"; // Component to display weather data
 import { getWeatherData } from "../service/Api";
 import Container from "@material-ui/core/Container";
 import Grid from "@material-ui/core/Grid";
